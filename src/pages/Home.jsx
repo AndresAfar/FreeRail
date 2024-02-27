7ecd12645764652199dab0b106a654e82d2f49e6
AndresAfar <afar2503@gmail.com>
2024-02-27 09:34:48 -0500

    modified styles

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,11 @@ import React from 'react'
 export const Home = () => {
   return (
     <div className='container'>
-        <h1>Hola</h1>
+        <div className='content-map pt-10 h-full w-full'>
+            <div className='map text-white border-2'>
+                <h1>Hola</h1>
+            </div>
+        </div>
     </div>
   )
 }
