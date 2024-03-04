d093e9ad7968a4888e8982d5ad1f40acc73740a8
AndresAfar <afar2503@gmail.com>
2024-03-04 09:03:16 -0500

    modified styles and add map leaflet

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,8 +7,8 @@ import "leaflet/dist/leaflet.css"
 
 import { MapContainer } from 'react-leaflet/MapContainer'
 import { TileLayer } from 'react-leaflet/TileLayer'
-import { useMap } from 'react-leaflet/hooks'
 import { Marker, Popup } from 'react-leaflet'
+import MarkerClusterGroup from 'react-leaflet-cluster'
 
 export const Home = () => {
 
@@ -16,19 +16,25 @@ export const Home = () => {
     {
       geocode: [4.754731743966792, -74.11157140094889],
       popUp: "SkatePark Fontanar del Rio"
+    },
+    {
+      geocode: [4.734866165612362, -74.10723110291629],
+      popUp: "SkatePark Parque Piloto"
     }
   ]
 
 
   return (
     <div className='container p-4'>
-        <div className='content-map pt-10 h-full w-full'>
+        <div className='content-map pt-5 h-full w-full'>
             <div className='map text-white bg-slate-600 border-2 rounded-md h-[530px]'>
               <MapContainer center={[4.662176276366342, -74.0888006455318]} zoom={6}>
                 <TileLayer
                   attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                   url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
+                <MarkerClusterGroup>
+
                 {markers.map(marker =>(
                   <Marker position={marker.geocode}>
                     <Popup>
@@ -36,15 +42,16 @@ export const Home = () => {
                     </Popup>
                   </Marker>
                 ))
-                }
+              }
+              </MarkerClusterGroup>
               </MapContainer>
             </div>
         </div>
-        <div className='content-spots h-full w-full mt-10'>
+        <div className='content-spots h-full w-full mt-10 '>
             <span className='text-white text-3xl my-2'>Spots</span>
             <SpotCard>
               {data.map(spot=>
-                <SpotsList name={spot.nombre} description={spot.descripcion}/>
+                <SpotsList name={spot.nombre} description={spot.descripcion} img={spot.img}/>
                 )}
             </SpotCard>
         </div>
