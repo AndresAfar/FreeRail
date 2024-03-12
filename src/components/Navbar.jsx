48fe41c8420c0ecd2701229dce3c9858e61ce4d1
AndresAfar <afar2503@gmail.com>
2024-03-12 12:30:50 -0500

    modified styles

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,37 +4,44 @@ export const Navbar = () => {
   return (
     <>
         <header className='bg-[#101419] border-b-2 border-[#293038]'>
-            <nav className='mx-auto flex max-w-7xl items-center justify-between py-3 lg:px-8'>
-                <div className="flex lg:flex-1">
-                    <a href="/" className="inline-flex p-1.5">
-                        <img className="h-11 w-auto" src="/src/assets/img/FreeRail_Logo.png" alt=""/>
-                        <span className="text-white text-xl lg:text-3xl mx-4 font-bold">FreeRail</span>
-                    </a>
-                </div>
-                <div className='flex md:hidden'>
-                    <button  type="button" className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700">
-                        <span className="sr-only">Open main menu</span>
-                        <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" aria-hidden="true">
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-                        </svg>
-                    </button>
-                </div>
-                <div className="hidden w-full md:block md:w-auto" id="navbar-default">
-                    <ul className="font-bold flex flex-col p-6 md:p-0 md:items-center mt-4 border border-[#e26767] rounded-lg md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0">
-                        <li>
-                            <div className='relative'>
-                                <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-                                    <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                                        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
-                                    </svg>
-                                </div>
-                                <input type="text" className='rounded-md border-0 w-60 py-1.5 pl-9 text-white bg-[#293038]' placeholder='Buscar'/>
-                            </div>
-                        </li>
-                    </ul>
-                </div>
+            
 
-            </nav>
+<nav class="">
+  <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+  <a href="/" class="flex items-center space-x-3 rtl:space-x-reverse">
+      <img src="/src/assets/img/FreeRail_Logo.png" class="h-11" alt="FreeRail Logo" />
+      <span class="text-white self-center text-2xl font-semibold whitespace-nowrap">FreeRail</span>
+  </a>
+  <div class="flex md:order-2">
+    <button type="button" data-collapse-toggle="navbar-search" aria-controls="navbar-search" aria-expanded="false" class="md:hidden text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5 me-1">
+      <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+      </svg>
+      <span class="sr-only">Search</span>
+    </button>
+    <div class="relative hidden md:block">
+      <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+        <svg class="w-4 h-4 text-gray-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+          <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+        </svg>
+        <span class="sr-only">Search icon</span>
+      </div>
+      <input type="text" id="search-navbar" class="block w-full p-2 ps-10 text-sm text-white border border-gray-300 rounded-lg bg-[#293038] focus:ring-blue-500 focus:border-blue-500" placeholder="Search..."/>
+    </div>
+  </div>
+    <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-search">
+      <div class="relative mt-3 md:hidden">
+        <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+          <svg class="w-4 h-4 text-gray-300 " aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+          </svg>
+        </div>
+        <input type="text" id="search-navbar" class="block w-full p-2 ps-10 text-sm text-white border border-gray-300 rounded-lg bg-[#293038] focus:ring-blue-500 focus:border-blue-500" placeholder="Search..."/>
+      </div>
+    </div>
+  </div>
+</nav>
+
         </header>
     </>
   )
