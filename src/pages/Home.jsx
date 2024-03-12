edff92283143f2e65cde7e6b4bbcccd8bb3c8a8e
AndresAfar <afar2503@gmail.com>
2024-03-12 10:54:47 -0500

    modified styles grid cards

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -30,7 +30,7 @@ export const Home = () => {
             <div className='map text-white bg-slate-600 border-2 rounded-md h-[530px]'>
               <MapContainer center={[4.662176276366342, -74.0888006455318]} zoom={11}>
                 <TileLayer
-                  attribution='&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                  attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                   url='https://tile.openstreetmap.org/{z}/{x}/{y}.png'
                 />
                 <MarkerClusterGroup>
@@ -49,8 +49,8 @@ export const Home = () => {
         <div className='content-bts w-full my-8 grid justify-items-end'>
           <a href="/new-spot" type='button' className=' text-white bg-[#293038] md:text-base p-3 rounded-lg'>Nuevo Spot</a>
         </div>
-        <div className='content-spots h-full w-full mt-10 '>
-            <span className='text-white text-3xl my-2'>Spots</span>
+        <div className='content-spots mt-8 '>
+          <span className='text-white text-3xl my-2'>Spots</span>
             <SpotCard>
               {data.map(spot=>
                 <SpotsList name={spot.nombre} description={spot.descripcion} img={spot.img} key={spot.id}/>
